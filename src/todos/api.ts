6d9f8074d0aa625677793924789cd904318bd640
上田貴央 <gibson511511@gmail.com>
2023-04-19 09:06:28 +0900

    add CRUD

---
+++ src/todos/api.ts
@@ -0,0 +1,58 @@
+import { Hono } from 'hono';
+
+// TODO: let to const
+let todoList = [
+	{ id: '1', title: 'Learning Hono', completed: false },
+	{ id: '2', title: 'Watch the movie', completed: true },
+	{ id: '3', title: 'Buy milk', completed: false },
+];
+
+// TODO: todos to app
+const todos = new Hono();
+todos.get('/', (c) => c.json(todoList));
+
+todos.post('/', async (c) => {
+	const param = await c.req.json<{ title: string }>();
+	const newTodo = {
+		id: String(todoList.length + 1),
+		completed: false,
+		title: param.title,
+	};
+	todoList = [...todoList, newTodo];
+
+	return c.json(newTodo, 201);
+});
+
+todos.put('/:id', async (c) => {
+	const id = c.req.param('id');
+	const todo = todoList.find((todo) => todo.id === id);
+	if (!todo) {
+		return c.json({ message: 'not found' }, 404);
+	}
+	const param = (await c.req.parseBody()) as {
+		title?: string;
+		completed?: boolean;
+	};
+	todoList = todoList.map((todo) => {
+		if (todo.id === id) {
+			return {
+				...todo,
+				...param,
+			};
+		} else {
+			return todo;
+		}
+	});
+	return new Response(null, { status: 204 });
+});
+
+todos.delete('/:id', async (c) => {
+	const id = c.req.param('id');
+	const todo = todoList.find((todo) => todo.id === id);
+	if (!todo) return c.json({ message: 'Not Found.' }, 404);
+
+	todoList = todoList.filter((todo) => todo.id !== id);
+	return new Response(null, { status: 204 });
+});
+
+export { todos };
