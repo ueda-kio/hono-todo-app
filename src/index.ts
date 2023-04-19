0c94a2487cbbf94819d6da0b59e12cb7b7cd56bb
上田貴央 <gibson511511@gmail.com>
2023-04-19 09:17:50 +0900

    add basicAuth

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { Hono } from 'hono';
+import { basicAuth } from 'hono/basic-auth';
 import { todos } from './todos/api';
 
 const app = new Hono();
 app.route('/api/todos', todos);
+
+app.use(
+	'/api/*',
+	basicAuth({
+		username: 'kio',
+		password: 'kio',
+	})
+);
 
 export default app;
